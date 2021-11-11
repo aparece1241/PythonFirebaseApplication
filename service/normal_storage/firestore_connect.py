024121f22267d8100d1466618e499bf5ba432455
Joe John Ferrolino <joejohnferrolino@gmail.com>
2021-11-11 19:05:30 +0800

    [UPDATE] partial CRUD setup

--- service/normal_storage/firestore_connect.py
+++ service/normal_storage/firestore_connect.py
@@ -6,3 +6,12 @@ class FireStoreStorage:
 
     def __init__(self, app):
         self.app = app
+        self.storage_instance = firestore.client(self.app)
+
+    def get_all(self, collection_name):
+        collection = self.storage_instance.collection('u{collection}'.format(collection_name)).get()
+        return collection
+
+
+if __name__ == '__main__':
+    print('This module in not supposed to run')
