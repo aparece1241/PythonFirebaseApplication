f5bda910893a3ab6742fbce1e4b3d2b782c60065
Joe John Ferrolino <joejohnferrolino@gmail.com>
2021-11-16 15:38:11 +0800

    [UPDATE] modifiy service to be base for models

--- service/normal_storage/firestore_connect.py
+++ service/normal_storage/firestore_connect.py
@@ -3,14 +3,11 @@ from firebase_admin import firestore
 
 
 class FireStoreStorage:
+    collection = None
 
     def __init__(self, app):
         self.app = app
         self.storage_instance = firestore.client(self.app)
-
-    def get_all(self, collection_name):
-        collection = self.storage_instance.collection('u{collection}'.format(collection_name)).get()
-        return collection
 
 
 if __name__ == '__main__':
