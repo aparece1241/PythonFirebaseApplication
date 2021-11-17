51e3448d2a8cfef4fb713e4ee4283e9995af36ce
Joe John Ferrolino <joejohnferrolino@gmail.com>
2021-11-17 10:45:38 +0800

    [SETUP] set view structure with tkinter

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+# Storage imports
 from service.firebase_connection import FirebaseConnection
 from service.normal_storage.firestore_connect import FireStoreStorage 
 
+# views imports
+from views.page_manager import PageManager
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    page_manager = PageManager()
+
     firebase_connection = FirebaseConnection('https://fir-withpython-9d955-default-rtdb.firebaseio.com/',
                        './authkey/google_firebase_key.json')
     fire_app = firebase_connection.connect()
     normal_storage = FireStoreStorage(fire_app)
-    users = normal_storage.get_all('users')
-    for user in users:
-        print('{firstname} {lastname}'.format(firstname = user.get('first'), lastname = user.get('last')))
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
